e14a033f0b43c2e85c7db85fdcdddca0b4c7a444
William Augusto <william.santos315@outlook.com>
2021-04-26 01:34:31 -0300

    axios configuration and .env

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 import store from './store'
+import './plugins/axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$api = process.env.VUE_APP_API_URL
 
 new Vue({
-  router,
+  // router,
   store,
   render: h => h(App)
 }).$mount('#app')
