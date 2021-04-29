de5e253ab9608dba715bac64e93aafeb8cf3b94f
William Augusto <william.santos315@outlook.com>
2021-04-29 16:36:27 -0300

    playlist design

--- src/routes/playlist.js
+++ src/routes/playlist.js
@@ -3,5 +3,11 @@ export default [
     path: '/playlists/create',
     name: 'PlaylistCreate',
     component: () => import(/* webpackChunkName: "playlist_create" */ '../views/Playlist/PlaylistCreate.vue')
+  },
+  {
+    path: '/playlists/:playlist',
+    name: 'Playlist',
+    props: true,
+    component: () => import(/* webpackChunkName: "playlist" */ '../views/Playlist/Playlist.vue')
   }
 ]
---
+++ src/views/Playlist/Playlist.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container content">
+    <h1 class="title">
+      Playlist
+    </h1>
+    <p class="about">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus dictum eu libero id gravida. Ut ac elementum lorem.
+    </p>
+    <p class="created">
+      Criado em 12/03/2021 • Alterado em 13/04/2021
+    </p>
+    <div class="main-container">
+      <ul>
+        <li v-wave>
+          <router-link to="/teste">
+            <p class="just-title episode-title">
+              <i class="fas fa-play" />
+              Conversa com pessoa X no dia Y
+            </p>
+            <p class="time">
+              01:15:30
+            </p>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Playlist X • Upcast'
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../../styles/show.scss';
+</style>
