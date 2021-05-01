93451821f3682f24cda1311d95229758d85d8c16
William Augusto <william.santos315@outlook.com>
2021-05-01 13:36:44 -0300

    create user => AXIOS POST

--- src/views/Account/SignUp.vue
+++ src/views/Account/SignUp.vue
@@ -8,27 +8,39 @@
     </router-link>
     <h1>Criar uma conta</h1>
     <input
+      v-model="email"
       type="email"
       placeholder="Adicione um e-mail"
     >
     <input
+      v-model="username"
       type="text"
       placeholder="Adicione um nome de usuário"
     >
     <input
+      v-model="password"
       type="password"
       placeholder="Adicione uma senha"
-    >
-    <input
-      type="password"
-      placeholder="Confirme a senha"
     >
     <button
       v-wave
       type="button"
+      :disabled="disabled"
+      @click="createUser"
     >
       Continuar
     </button>
+    <ul
+      v-if="errors.length > 0"
+      class="error-list"
+    >
+      <li
+        v-for="(error, index) in errors"
+        :key="index"
+      >
+        {{ error }}
+      </li>
+    </ul>
     <router-link
       class="link"
       :to="{ name: 'SignIn' }"
@@ -42,6 +54,34 @@
 export default {
   metaInfo: {
     title: 'Criar conta gratuita • Upcast'
+  },
+  data () {
+    return {
+      email: null,
+      username: null,
+      password: null,
+      errors: [],
+      disabled: false
+    }
+  },
+  methods: {
+    async createUser () {
+      const { email, username, password, $axios, $router } = this
+      const data = { email, username, password }
+
+      this.errors = []
+      this.disabled = true
+
+      try {
+        await $axios.post('/users', data)
+
+        $router.push({ name: 'SignIn' })
+      } catch (err) {
+        this.errors = err.response.data
+      } finally {
+        this.disabled = false
+      }
+    }
   }
 }
 </script>
