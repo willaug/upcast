3da88893f329be60462a714afbe74264ab833543
William Augusto <william.santos315@outlook.com>
2021-05-03 14:40:36 -0300

    password reset (add new pass)

--- src/views/Account/PasswordReset/AddNewPassword.vue
+++ src/views/Account/PasswordReset/AddNewPassword.vue
@@ -7,20 +7,53 @@
       >
     </router-link>
     <h1>Adicionar nova senha</h1>
+    <transition
+      name="fade"
+    >
+      <ul
+        v-if="verificationErrors.length > 0"
+        class="error-list"
+      >
+        <li
+          v-for="(error, index) in verificationErrors"
+          :key="index"
+        >
+          {{ error }}
+        </li>
+      </ul>
+    </transition>
     <input
+      v-model="password"
       type="password"
       placeholder="Adicione uma nova senha"
     >
     <input
+      v-model="confirmPassword"
       type="password"
       placeholder="Confirme a nova senha"
     >
     <button
       v-wave
       type="button"
+      @click="addNewPassword"
     >
       Continuar
     </button>
+    <transition
+      name="fade"
+    >
+      <ul
+        v-if="errors.length > 0"
+        class="error-list"
+      >
+        <li
+          v-for="(error, index) in errors"
+          :key="index"
+        >
+          {{ error }}
+        </li>
+      </ul>
+    </transition>
   </form>
 </template>
 
@@ -28,6 +61,67 @@
 export default {
   metaInfo: {
     title: 'Adicionar nova senha • Upcast'
+  },
+  props: {
+    passwordReset: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      verificationErrors: [],
+      errors: [],
+      password: null,
+      confirmPassword: null
+    }
+  },
+  created () {
+    const { passwordReset, $axios } = this
+
+    $axios(`/password-reset/${passwordReset}`)
+      .catch(err => {
+        if (err.response) {
+          const response = err.response.data
+          const responseIsArray = Array.isArray(response)
+
+          if (responseIsArray) {
+            this.verificationErrors = response
+          } else {
+            this.verificationErrors.push(response)
+          }
+        } else {
+          const message = 'Ocorreu um erro de conexão. Tente novamente mais tarde!'
+          this.verificationErrors.push(message)
+        }
+      })
+  },
+  methods: {
+    addNewPassword () {
+      const { passwordReset, password, confirmPassword, $axios, $router } = this
+      const data = { password, confirmPassword }
+
+      this.verificationErrors = []
+      this.errors = []
+
+      $axios.patch(`/password-reset/${passwordReset}`, data)
+        .then(() => $router.push({ name: 'SignIn' }))
+        .catch(err => {
+          if (err.response) {
+            const response = err.response.data
+            const responseIsArray = Array.isArray(response)
+
+            if (responseIsArray) {
+              this.errors = response
+            } else {
+              this.errors.push(response)
+            }
+          } else {
+            const message = 'Ocorreu um erro de conexão. Tente novamente mais tarde!'
+            this.errors.push(message)
+          }
+        })
+    }
   }
 }
 </script>
--- src/views/Account/PasswordReset/PasswordReset.vue
+++ src/views/Account/PasswordReset/PasswordReset.vue
@@ -8,15 +8,42 @@
     </router-link>
     <h1>Recuperar conta</h1>
     <input
+      v-model="email"
       type="email"
       placeholder="Adicione um e-mail"
     >
     <button
       v-wave
       type="button"
+      @click="addPasswordReset"
     >
       Continuar
     </button>
+    <transition
+      name="fade"
+    >
+      <div
+        v-if="success !== null"
+        class="success"
+      >
+        {{ success }}
+      </div>
+    </transition>
+    <transition
+      name="fade"
+    >
+      <ul
+        v-if="errors.length > 0"
+        class="error-list"
+      >
+        <li
+          v-for="(error, index) in errors"
+          :key="index"
+        >
+          {{ error }}
+        </li>
+      </ul>
+    </transition>
   </form>
 </template>
 
@@ -24,6 +51,41 @@
 export default {
   metaInfo: {
     title: 'Recuperar conta • Upcast'
+  },
+  data () {
+    return {
+      email: null,
+      success: null,
+      errors: []
+    }
+  },
+  methods: {
+    addPasswordReset () {
+      const { email, $axios } = this
+      const data = { email }
+      this.errors = []
+      this.success = null
+
+      $axios.post('password-reset', data)
+        .then(result => {
+          this.success = result.data
+        })
+        .catch(err => {
+          if (err.response) {
+            const response = err.response.data
+            const responseIsArray = Array.isArray(response)
+
+            if (responseIsArray) {
+              this.errors = response
+            } else {
+              this.errors.push(response)
+            }
+          } else {
+            const message = 'Ocorreu um erro de conexão. Tente novamente mais tarde!'
+            this.errors.push(message)
+          }
+        })
+    }
   }
 }
 </script>
