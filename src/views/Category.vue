7f6960a63b16a2963dafbfd1009a684bcaf91157
William Augusto <william.santos315@outlook.com>
2021-05-03 17:34:03 -0300

    get category shows

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,21 +1,42 @@
 <template>
   <div class="container content">
     <div class="main-container">
-      <h1 class="title">
-        Categoria X
-      </h1>
-      <h2>Conheça programas</h2>
-      <ul>
-        <li v-wave>
-          <router-link to="/teste">
-            <img
-              src="https://picsum.photos/320"
-              alt="Miniatura do programa"
+      <template v-if="categoryName === null || categoryName === undefined">
+        <div class="one-error">
+          {{ error }}
+        </div>
+      </template>
+      <template v-else>
+        <h1 class="title">
+          {{ categoryName }}
+        </h1>
+        <template v-if="shows.length > 0">
+          <h2>Conheça programas</h2>
+          <ul>
+            <li
+              v-for="(show, index) in shows"
+              :key="index"
+              v-wave
             >
-            <p>Flow Podcast</p>
-          </router-link>
-        </li>
-      </ul>
+              <router-link :to="{ name: 'Show', params: { show: show.uid } }">
+                <div class="photo">
+                  <img
+                    :src="$api + show.url_photo"
+                    :class="{ photoIcon: show.url_photo.includes('.svg') }"
+                    :alt="show.title"
+                  >
+                </div>
+                <p>{{ show.title }}</p>
+              </router-link>
+            </li>
+          </ul>
+        </template>
+        <template v-else>
+          <div class="one-error message">
+            Ainda não há programas nesta categoria.
+          </div>
+        </template>
+      </template>
     </div>
   </div>
 </template>
@@ -24,6 +45,37 @@
 export default {
   metaInfo: {
     title: 'Categoria X • Upcast'
+  },
+  props: {
+    category: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      categoryName: null,
+      error: null,
+      shows: []
+    }
+  },
+  async created () {
+    const { category, $axios } = this
+    this.error = null
+
+    try {
+      const categoryFound = await $axios(`categories/${category}`)
+      this.categoryName = categoryFound.data.response.name
+
+      const showsFound = await $axios(`categories/${category}/shows`)
+      this.shows = showsFound.data.response
+    } catch (err) {
+      if (err.response) {
+        this.error = err.response.data
+      } else {
+        this.error = 'Ocorreu um erro de conexão. Tente novamente mais tarde!'
+      }
+    }
   }
 }
 </script>
